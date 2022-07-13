b34bc5144a3a1c06a90ff70259f2f652d540e4e2
Thinhle1308 <doanthinh1308@gmail.com>
2022-07-13 15:30:32 +0700

    create mobile shop

---
+++ src/JS/slick.js
@@ -0,0 +1,10 @@
+$('.image-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  draggable: true, arrows: true,
+  prevArrow: `<button type='button' class='slick-prev pull- left'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
+  nextArrow: `<button type='button' class='slick-next pull - right'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
+});
+
