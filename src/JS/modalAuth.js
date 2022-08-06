699e4f2a070f250022adeec430cd33bcb33c72c4
Thinhle1308 <doanthinh1308@gmail.com>
2022-08-06 20:29:52 +0700

    mobile

---
+++ src/JS/modalAuth.js
@@ -0,0 +1,24 @@
+
+
+
+
+const user = document.querySelector(".user");
+const userModal = document.querySelector(".user-moder")
+
+user.addEventListener("click", () => {
+    if (userModal.classList.contains("show")) {
+        userModal.classList.remove("show")
+    }
+    else {
+        userModal.classList.add("show")
+    }
+})
+
+
+// window.onclick = function(event) {
+//     if(!event.target.matches(".user") ) {
+//         userModal.classList.remove("show") ;
+//     }
+//  }
+
+
